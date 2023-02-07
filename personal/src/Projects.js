390dea2da28562990df43af578684a43a8e47943
Aniketdange23 <aniketdange23212gmail.com>
2023-02-07 09:22:32 +0530

    init

---
+++ personal/src/Projects.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <div>
+        {/* project cards */}
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        {/* ... */}
+      </div>
+    </section>
+  );
+}
+
+function ProjectCard() {
+  return (
+    <div>
+      {/* project card content */}
+    </div>
+  );
+}
+
+export default Projects;
